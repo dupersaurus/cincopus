d13e460004d7372a475b90747e5cc2eea203265d
Jason DuPertuis <gallahad@me.com>
2015-05-19 23:06:55 -0400

    Bridging header

---
+++ CricketUmp/CricketUmp/CricketUmp-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  CricketUmp-Bridging-Header.h
+//  CricketUmp
+//
+//  Created by Jason DuPertuis on 5/19/15.
+//  Copyright (c) 2015 jdp. All rights reserved.
+//
+
+#import "NSString+FontAwesome.h"
